0e65be8ff5a08a5d8b7ec833388e48b7502180c1
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-06-08 00:09:51 -0300

    tipos

--- lib/model/pokemon.dart
+++ lib/model/pokemon.dart
@@ -2,22 +2,37 @@ class Pokemon {
   final int id;
   final String name;
   final String image;
-  final String type;
+  final List<String> type;
   final int hp;
   final int attack;
   final int defense;
 
-  const Pokemon({required this.id, required this.name, required this.image, required this.type, required this.hp, required this.attack, required this.defense});
+  const Pokemon(
+      {required this.id,
+      required this.name,
+      required this.image,
+      required this.type,
+      required this.hp,
+      required this.attack,
+      required this.defense});
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(
       id: json['id'],
       name: json['name'],
       image: json['sprites']['other']['dream_world']['front_default'],
-      type: json['types'][0]['type']['name'],
+      type: getPokeTypes(json),
       hp: json['stats'][0]['base_stat'],
       attack: json['stats'][1]['base_stat'],
       defense: json['stats'][2]['base_stat'],
     );
   }
+}
+
+List<String> getPokeTypes(Map<String, dynamic> json) {
+  List<String> list = [];
+  for (var element in json['types']) {
+    list.add(element['type']['name']);
+  }
+  return list;
 }
